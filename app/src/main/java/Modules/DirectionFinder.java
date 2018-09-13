3b0cda36722c56687c593fc59cbcc9c568300b3e
Mehdi-Shah <mehdi.shah563@gmail.com>
2018-09-13 21:14:48 +1200

    fixed fake exercise bugs

--- app/src/main/java/Modules/DirectionFinder.java
+++ app/src/main/java/Modules/DirectionFinder.java
@@ -53,30 +53,33 @@ public class DirectionFinder  {
             distance = Integer.parseInt(destination);
             if (distance < 4) {
                 urlDestination = "Wintergarden Rd, Parnell, Auckland 1010"; //2k
+                return DIRECTION_URL_API + "&origin=" + urlOrigin + "&destination=" + urlOrigin + "&alternatives=true" + "&mode=bicycling&waypoints="+ urlDestination + "&key=" + GOOGLE_API_KEY;
+
             }
             else if (distance < 6) {
-                urlDestination = "Ayr Reserve, Parnell";
+                urlDestination = "Ayr Reserve, Parnell"; //4k
+                return DIRECTION_URL_API + "&origin=" + urlOrigin + "&destination=" + urlOrigin + "&alternatives=true" + "&mode=bicycling&waypoints="+ urlDestination + "&key=" + GOOGLE_API_KEY;
+
             }
             else if (distance < 8) {
                 if (distance > 6.8) {
                     urlDestination = "19 Windmill Rd, Mount Eden, Auckland 1024"; //7k
+                    return DIRECTION_URL_API + "&origin=" + urlOrigin + "&destination=" + urlOrigin + "&alternatives=true" + "&mode=bicycling&waypoints="+ urlDestination + "|20+Maungawhau+Rd,+Epsom,+Auckland&key=" + GOOGLE_API_KEY;
+
                 }
                 else {
                     urlDestination = "Albert Park, Bowen Ln, Auckland"; //6k
+                    return DIRECTION_URL_API + "&origin=" + urlOrigin + "&destination=" + urlOrigin + "&alternatives=true" + "&mode=bicycling&waypoints="+ urlDestination + "&key=" + GOOGLE_API_KEY;
+
                 }
             }
             else if (distance < 9) {
                 urlDestination = "Grey Lynn Park, 75 Dryden St, Grey Lynn, Auckland 1021"; // 8k
+                return DIRECTION_URL_API + "&origin=" + urlOrigin + "&destination=" + urlOrigin + "&alternatives=true" + "&mode=bicycling&waypoints="+ urlDestination + "&key=" + GOOGLE_API_KEY;
             }
             else {
                 urlDestination = "Cornwall Park, Green Ln W, Epsom, Auckland 1051"; //10k
-            }
-
-            if (distance > 8) {
                 return DIRECTION_URL_API + "&origin=" + urlOrigin + "&destination=" + urlOrigin + "&alternatives=true" + "&mode=bicycling&waypoints="+ urlDestination + "|20+Maungawhau+Rd,+Epsom,+Auckland&key=" + GOOGLE_API_KEY;
-            }
-            else {
-                return DIRECTION_URL_API + "&origin=" + urlOrigin + "&destination=" + urlOrigin + "&alternatives=true" + "&mode=bicycling&waypoints="+ urlDestination + "|98+park+rd+grafton&key=" + GOOGLE_API_KEY;
             }
         }
         else {
